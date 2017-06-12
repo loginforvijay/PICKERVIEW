afe68b08b97ca52e9c6ba59d48b50d46dce3b77b
Admin <admin@Admins-Mac.local>
2017-06-12 10:54:28 +0530

    picker view commit

--- PickerViewTwo/ViewController.swift
+++ PickerViewTwo/ViewController.swift
@@ -8,11 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate,UIPickerViewDataSource
 
+{
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var pickerview: UIPickerView!
+    
+    
+    let names = ["India","Pakistan","Afganistan","Burma","Butan","Maldives","Nepal","Srilanka"]
+    
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+      return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return names.count
+    }
+    
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return names[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int)
+    {
+        label.text =  names[row]
+    
+    self.view.endEditing(false)
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+   
+    
+        
+        
+        
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
